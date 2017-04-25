cd22c4b7787929760d7c779498c6a35c6195e239
willh99 <willh99@gwmail.gwu.edu>
2017-04-25 12:10:26 -0400

    Minor Formatting Changes

--- Alarm_Clock/alarm_clock.v
+++ Alarm_Clock/alarm_clock.v
@@ -21,11 +21,11 @@
 module alarm_clock(
     output [6:0] sseg,
     output [3:0] dgt,
-	 output pm,
+    output pm,
     input [3:0] bcd,
-	 input [3:0] load,
-	 input sel24,
-	 input clock
+    input [3:0] load,
+    input sel24,
+    input clock
     );
 
 	wire [6:0] sseg1;
@@ -50,11 +50,10 @@ module alarm_clock(
 				
 	always @(posedge clock)
 	   begin
-		   prescaler = prescaler + 1;
+		prescaler = prescaler + 1;
 			if (prescaler>100000) 
 				update_scan;
 		end
-	
 	   
 	assign sseg = ~sseg1;		
 
@@ -64,7 +63,7 @@ task update_scan;
 		state = state + 1;
 		scan = scan + 1;
 		if (scan>100) begin
-         scan = 0;
+         		scan = 0;
 			tick = tick + 1;
 			adj_process;
 
@@ -109,56 +108,54 @@ endtask
 
 task inc_min1 (inout reg [3:0] min1);	   
    begin
-		min1 = min1 + 1;
-		if (min1>9) min1 = 0;
-	end
+	min1 = min1 + 1;
+	if (min1>9) min1 = 0;
+    end
 endtask
 
 task inc_min10 (inout reg [3:0] min10);	   
    begin
-		min10 = min10 + 1;
-		if (min10>5) min10 = 0;				
-	end
+	min10 = min10 + 1;
+	if (min10>5) min10 = 0;				
+    end
 endtask
 
 task inc_hour1 (inout reg [3:0] hour1);	   
    begin
-		hour1 = hour1 + 1;
-		if (hour1>9)  hour1 = 0;
-	end
+	hour1 = hour1 + 1;
+	if (hour1>9)  hour1 = 0;
+   end
 endtask
 
 task inc_hour10 (inout reg [3:0] hour10);	   
    begin
-		hour10 = hour10 + 1;
-		if (hour10>2) hour10 = 0;
-	end
+	hour10 = hour10 + 1;
+	if (hour10>2) hour10 = 0;
+   end
 endtask
 
 task adj_process;
 	if (bcd[3:2]==0)
-		begin
-		   if (load[3]) inc_hour10(time24[3]);
-		   if (load[2]) inc_hour1(time24[2]);
-		   if (load[1]) inc_min10(time24[1]);
-		   if (load[0]) inc_min1(time24[0]);
-		end
+	begin
+		if (load[3]) inc_hour10(time24[3]);
+		if (load[2]) inc_hour1(time24[2]);
+		if (load[1]) inc_min10(time24[1]);
+		if (load[0]) inc_min1(time24[0]);
+	end
 	else if (bcd[3:2]==1)
-		begin
-		   if (load[3]) inc_hour10(alarm1[3]);
-		   if (load[2]) inc_hour1(alarm1[2]);
-		   if (load[1]) inc_min10(alarm1[1]);
-		   if (load[0]) inc_min1(alarm1[0]);
-		end		
+	begin
+		if (load[3]) inc_hour10(alarm1[3]);
+		if (load[2]) inc_hour1(alarm1[2]);
+		if (load[1]) inc_min10(alarm1[1]);
+		if (load[0]) inc_min1(alarm1[0]);
+	end		
 	else if (bcd[3:2]==2)
-		begin
-		   if (load[3]) inc_hour10(alarm2[3]);
-		   if (load[2]) inc_hour1(alarm2[2]);
-		   if (load[1]) inc_min10(alarm2[1]);
-		   if (load[0]) inc_min1(alarm2[0]);
-		end				
+	begin
+		if (load[3]) inc_hour10(alarm2[3]);
+		if (load[2]) inc_hour1(alarm2[2]);
+		if (load[1]) inc_min10(alarm2[1]);
+		if (load[0]) inc_min1(alarm2[0]);
+	end				
 endtask
 
 endmodule
-
-
